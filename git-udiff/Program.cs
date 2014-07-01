8f359d0ee62ae7205e74a6adf81ad74a800ca148
rbaker <russell.baker@mlp.com>
2014-07-01 15:32:01 -0400

    init

---
+++ git-udiff/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace git_udiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I work with args");
+            foreach (var s in args)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
