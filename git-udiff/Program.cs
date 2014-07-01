61d217eec56c10e358bf9a0317acdd47e15a6f57
russell.j.baker@gmail.com <russell.j.baker@gmail.com>
2014-07-01 19:22:10 -0400

    sending to desktop

--- git-udiff/Program.cs
+++ git-udiff/Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using GitSharp;
+using System.IO;
 
 namespace git_udiff
 {
     class Program
     {
+	static string tempDir;
         static void Main(string[] args)
         {
-            var repo = new Repository(Environment.CurrentDirectory);
-            var i = new Index(repo);
-
-
-            Console.WriteLine(Environment.CurrentDirectory);
-            foreach (var conflict in repo.Status.MergeConflict)
+            using (var repo = new Repository(Environment.CurrentDirectory))
             {
-                Console.WriteLine(conflict);
+                Console.WriteLine(Environment.CurrentDirectory);
+                foreach (var conflict in repo.Status.MergeConflict)
+                {
+                    Console.WriteLine(conflict);
+                }
             }
+            SetupTempFolder();
+        }
+
+        static void SetupTempFolder()
+        {
+            var contentDirs = Directory.GetDirectories(Environment.CurrentDirectory, "Content");
+
+	    //TODO: turn this into an error that asks the user what directory they want to look in.
+            if (contentDirs.Length != 1)
+            {
+                throw new Exception("Could not find content directory. Make sure you are in a UE4 project and are at the root directory.");
+            }
+
+            tempDir = Directory.CreateDirectory(Path.Combine(contentDirs[0], "UDiff")).FullName;
+
+            Console.WriteLine(tempDir);
         }
     }
 }
